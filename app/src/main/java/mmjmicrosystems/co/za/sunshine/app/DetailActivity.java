387bccacd4dfb2e601becc92ce2cd1c5d61fad72
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-02-28 05:45:54 +0200

    Display weather forecast on the Details Fragment'

--- app/src/main/java/mmjmicrosystems/co/za/sunshine/app/DetailActivity.java
+++ app/src/main/java/mmjmicrosystems/co/za/sunshine/app/DetailActivity.java
@@ -1,5 +1,6 @@
 package mmjmicrosystems.co.za.sunshine.app;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -56,7 +58,13 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                TextView detailText = (TextView) rootView.findViewById(R.id.detail_text);
+                detailText.setText(forecast);
+            }
             return rootView;
         }
     }
